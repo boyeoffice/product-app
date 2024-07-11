bb1e4afe387a64fb61d5db8bb207a4be501f039b
Olakunle Boye <kunlexzy@gmail.com>
2024-07-11 05:57:02 +0100

    retrive product data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,28 +6,40 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api', name: 'api_')]
 class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index', methods: 'GET')]
-    public function index(): JsonResponse
+    public function index(EntityManagerInterface $entityManagerInterface): JsonResponse
     {
+        $products = $entityManagerInterface->getRepository(Product::class)->findAll();
+
+        $data = [];
+
+        foreach ($products as $product):
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'description' => $product->getDescription()
+            ];
+        endforeach;
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
+            'data' => $data,
         ]);
     }
 
     #[Route('/products', name: 'product_store', methods: 'POST')]
-    public function store(EntityManagerInterface $entityManager): JsonResponse
+    public function store(EntityManagerInterface $entityManager, Request $request): JsonResponse
     {
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
-        $product->setQuantity(2);
-        $product->setDescription('Ergonomic and stylish!');
+        $product->setName($request->request->get('name'));
+        $product->setPrice($request->request->get('price'));
+        $product->setQuantity($request->request->get('quantity'));
+        $product->setDescription($request->request->get('description'));
 
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($product);
